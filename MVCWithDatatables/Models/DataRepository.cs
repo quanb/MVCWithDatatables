b3f9f3cbe9c8c1318b5052f5f0469e654783ade8
quanb <quantrinhbao@gmail.com>
2015-07-08 20:50:52 +0700

    Global filter and paging     Fix bug get search value in binder     Use dynamic linq  to filter     Paging

--- MVCWithDatatables/Models/DataRepository.cs
+++ MVCWithDatatables/Models/DataRepository.cs
@@ -1,9 +1,10 @@
-﻿using NHibernate;
+﻿using MVCWithDatatables.Models.DataTables;
+using NHibernate;
 using NHibernate.Linq;
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
+using System.Linq.Dynamic;
+using System.Text;
 
 namespace MVCWithDatatables.Models
 {
@@ -16,13 +17,73 @@ namespace MVCWithDatatables.Models
             _session = session;
         }
 
-        public IList<Company> Companies()
+        public IList<Company> Companies(DataTableParamModel tableParam)
         {
-            var companies = _session.Query<Company>()
-                                  .OrderByDescending(p => p.Name)
-                                  .ToList();
-            return companies;
+            IQueryable<Company> companies;
+            if (!string.IsNullOrEmpty(tableParam.Search.Value))
+            {
+                var filterCol = from i in tableParam.Columns
+                                where i.Searchable
+                                select i.Name;
+
+                StringBuilder filterString =  new StringBuilder();
+                bool beginFlag = true;
+                foreach (string col in filterCol) // Loop through all strings
+                {
+                    if(beginFlag)
+                    {
+                        beginFlag = false;
+                    }
+                    else
+                    {
+                        filterString.Append(" Or ");
+                    }
+                    filterString.AppendFormat("{0}.ToLower().Contains(@0)", col);
+                }
+                companies = _session.Query<Company>()
+                                .Where(filterString.ToString(), tableParam.Search.Value.ToLower())
+                                .Skip(tableParam.Start)
+                                .Take(tableParam.Length);
+            }
+            else
+            {
+                companies = _session.Query<Company>()
+                               .Skip(tableParam.Start)
+                               .Take(tableParam.Length);
+            }
+            return companies.ToList();
         }
 
+        public int TotalCompanies()
+        {
+            return _session.Query<Company>().Count();
+        }
+
+        public int TotalCompaniesFiltered(DataTableParamModel tableParam)
+        {
+            if (!string.IsNullOrEmpty(tableParam.Search.Value))
+            {
+                var filterCol = from i in tableParam.Columns
+                                where i.Searchable
+                                select i.Name;
+                StringBuilder filterString = new StringBuilder();
+                bool beginFlag = true;
+                foreach (string col in filterCol) // Loop through all strings
+                {
+                    if (beginFlag)
+                    {
+                        beginFlag = false;
+                    }
+                    else
+                    {
+                        filterString.Append(" Or ");
+                    }
+                    filterString.AppendFormat("{0}.ToLower().Contains(@0)", col);
+                }
+                return _session.Query<Company>()
+                                .Where(filterString.ToString(), tableParam.Search.Value.ToLower()).Count();
+            }
+            return _session.Query<Company>().Count();
+        }
     }
 }
